5f5d8762a5b45ca78030a373c002c1384a21a3ba
홍혁준 <jjj8337721@naver.com>
2023-02-02 23:26:07 +0900

    feat : damda product 구축

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # damda-product
+> MSA 기반의 사이드 프로젝트입니다.
+> Product Server 입니다.
+
+- 프로젝트 기간 : 2023.02.01 - continue
+
+# 프로젝트 기록
+<ol>
+<h3>23/02/02</h3>
+  <li>Product Server 생성</li>
+</ol>
---
+++ src/main/java/kr/co/damdaproduct/DamdaProductApplication.java
@@ -0,0 +1,13 @@
+package kr.co.damdaproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamdaProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamdaProductApplication.class, args);
+    }
+
+}
