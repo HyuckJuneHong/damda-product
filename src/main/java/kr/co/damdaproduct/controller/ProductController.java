a05b9e38bd7bb99283c53f2e24a666bf848db59b
홍혁준 <jjj8337721@naver.com>
2023-02-18 10:46:00 +0900

    feat : product api

--- src/main/java/kr/co/damdaproduct/controller/ProductController.java
+++ src/main/java/kr/co/damdaproduct/controller/ProductController.java
@@ -1,13 +1,12 @@
 package kr.co.damdaproduct.controller;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.damdaproduct.dto.ResponseDto;
 import kr.co.damdaproduct.error.model.ResponseFormat;
 import kr.co.damdaproduct.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +18,23 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @ApiOperation("상품 등록 / 관리자만")
+    @PostMapping
+    public ResponseFormat<String> createProduct(){
+        //TODO create product api
+        return null;
+    }
+
+    @ApiOperation("모든 상품 정보 조회")
     @GetMapping("/all")
     public ResponseFormat<List<ResponseDto.READ_PRODUCT_INFO>> getAllProductsInfo(){
         return ResponseFormat.ok(productService.getAllProductsInfo());
+    }
+
+    @ApiOperation("상품 삭제 / 관리자만")
+    @DeleteMapping
+    public ResponseFormat<String> deleteProduct(){
+        // TODO delete product service
+        return null;
     }
 }
